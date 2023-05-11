4d12ce7c619cc595670a846edd5c6dbbcc1d18ae
nanjingblue <mayitrust@gmail.com>
2023-05-11 20:13:59 +0800

    day2 单机并发缓存

---
+++ geecache.go
@@ -0,0 +1,102 @@
+package cache_go
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+/*
+如果缓存不存在，应从数据源（文件，数据库等）获取数据并添加到缓存中。
+GeeCache 是否应该支持多种数据源的配置呢？
+不应该，一是数据源的种类太多，没办法一一实现；
+二是扩展性不好。如何从源头获取数据，应该是用户决定的事情，
+我们就把这件事交给用户好了。因此，我们设计了一个回调函数(callback)，
+在缓存不存在时，调用这个函数，得到源数据
+*/
+
+/*
+定义接口 Getter 和 回调函数 Get(key string)([]byte, error)，参数是 key，返回值是 []byte。
+定义函数类型 GetterFunc，并实现 Getter 接口的 Get 方法。
+函数类型实现某一个接口，称之为接口型函数，方便使用者在调用时既能够传入函数作为参数，也能够传入实现了该接口的结构体作为参数。
+*/
+
+// A Getter loads data for a key.
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+// A GetterFunc implements Getter with a function.
+type GetterFunc func(key string) ([]byte, error)
+
+// Get implements Getter interface function
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+// A Group is a cache namespace and associated data loaded spread over
+type Group struct {
+	name      string
+	getter    Getter // callback to get the source data when the cache is not hit
+	mainCache cache
+}
+
+var (
+	mu     sync.Mutex
+	groups = make(map[string]*Group)
+)
+
+// NewGroup create a new instance of Group
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+// GetGroup returns the named group previously created with NewGroup, or
+// nil if there's no such group
+func GetGroup(name string) *Group {
+	mu.Lock()
+	g := groups[name]
+	mu.Unlock()
+	return g
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache] hit")
+		return v, nil
+	}
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
