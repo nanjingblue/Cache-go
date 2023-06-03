3ea23db59db7b509589309047fa437193f3b715d
nanjingblue <mayitrust@gmail.com>
2023-06-03 12:33:54 +0800

    day5 分布式节点

--- geecache/geecache.go
+++ geecache/geecache.go
@@ -39,6 +39,7 @@ type Group struct {
 	name      string
 	getter    Getter // callback to get the source data when the cache is not hit
 	mainCache cache
+	peers     PeerPicker
 }
 
 var (
@@ -71,6 +72,13 @@ func GetGroup(name string) *Group {
 	return g
 }
 
+func (g *Group) RegisterPeers(peers PeerPicker) {
+	if g.peers != nil {
+		panic("RegisterPeerPicker called more than once")
+	}
+	g.peers = peers
+}
+
 func (g *Group) Get(key string) (ByteView, error) {
 	if key == "" {
 		return ByteView{}, fmt.Errorf("key is required")
@@ -84,7 +92,23 @@ func (g *Group) Get(key string) (ByteView, error) {
 }
 
 func (g *Group) load(key string) (value ByteView, err error) {
+	if g.peers != nil {
+		if peer, ok := g.peers.PickPeer(key); ok {
+			if value, err := g.getFromPeer(peer, key); err == nil {
+				return value, nil
+			}
+		}
+		log.Println("[GeeCache] Failed to get from peer", err)
+	}
 	return g.getLocally(key)
+}
+
+func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
+	bytes, err := peer.Get(g.name, key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	return ByteView{bytes}, nil
 }
 
 func (g *Group) getLocally(key string) (ByteView, error) {
--- geecache/http.go
+++ geecache/http.go
@@ -2,18 +2,31 @@ package geecache
 
 import (
 	"fmt"
+	"geecache/geecache/consistenthash"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
 )
 
-const defaultBasePath = "/_geecache"
+const (
+	defaultBasePath = "/_geecache"
+	defaultReplicas = 50
+)
+
+var _ PeerPicker = (*HTTPPool)(nil)
+var _ PeerGetter = (*httpGetter)(nil)
 
 // HTTPPool implements PeerPicker for a pool of HTTP peers.
 type HTTPPool struct {
 	// this peer's base URL, e.g. "https://example.net:8000"
-	self     string
-	basePath string
+	self        string
+	basePath    string
+	mu          sync.Mutex // guards peers and httpGetters
+	peers       *consistenthash.Map
+	httpGetters map[string]*httpGetter // keyed by e.g. "http://10.0.0.2:8008" httpGetters，映射远程节点与对应的 httpGetter
 }
 
 // NewHTTPPool initializes an HTTP pool of peers.
@@ -59,4 +72,51 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(view.ByteSlice())
+}
+
+func (p *HTTPPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetters = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetters[peer] = &httpGetter{baseURL: peer + p.basePath}
+	}
+}
+
+func (p *HTTPPool) PickPeer(key string) (peer PeerGetter, ok bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetters[peer], true
+	}
+	return nil, false
+}
+
+type httpGetter struct {
+	baseURL string
+}
+
+func (h *httpGetter) Get(group, key string) ([]byte, error) {
+	u := fmt.Sprintf("%v%v%v",
+		h.baseURL,
+		url.QueryEscape(group),
+		url.QueryEscape(key),
+	)
+	res, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", res.StatusCode)
+	}
+	bytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading responsed error: %v", err)
+	}
+	return bytes, nil
 }
