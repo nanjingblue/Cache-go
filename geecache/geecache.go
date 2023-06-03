7d0b57007ce58d642ee9634a32bfce1933252d29
nanjingblue <mayitrust@gmail.com>
2023-06-03 15:21:45 +0800

    day6 防止缓存击穿

--- geecache/geecache.go
+++ geecache/geecache.go
@@ -2,6 +2,7 @@ package geecache
 
 import (
 	"fmt"
+	"geecache/geecache/singleflight"
 	"log"
 	"sync"
 )
@@ -40,6 +41,9 @@ type Group struct {
 	getter    Getter // callback to get the source data when the cache is not hit
 	mainCache cache
 	peers     PeerPicker
+	// use singleflight.Group to make sure that
+	// each key is only fetched once
+	loader *singleflight.Group
 }
 
 var (
@@ -58,6 +62,7 @@ func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 		name:      name,
 		getter:    getter,
 		mainCache: cache{cacheBytes: cacheBytes},
+		loader:    &singleflight.Group{},
 	}
 	groups[name] = g
 	return g
@@ -92,15 +97,21 @@ func (g *Group) Get(key string) (ByteView, error) {
 }
 
 func (g *Group) load(key string) (value ByteView, err error) {
-	if g.peers != nil {
-		if peer, ok := g.peers.PickPeer(key); ok {
-			if value, err := g.getFromPeer(peer, key); err == nil {
-				return value, nil
+	viewi, err := g.loader.Do(key, func() (interface{}, error) {
+		if g.peers != nil {
+			if peer, ok := g.peers.PickPeer(key); ok {
+				if value, err := g.getFromPeer(peer, key); err == nil {
+					return value, nil
+				}
 			}
+			log.Println("[GeeCache] Failed to get from peer", err)
 		}
-		log.Println("[GeeCache] Failed to get from peer", err)
+		return g.getLocally(key)
+	})
+	if err == nil {
+		return viewi.(ByteView), nil
 	}
-	return g.getLocally(key)
+	return
 }
 
 func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
